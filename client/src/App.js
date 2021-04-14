5fa45b4ee16569ff119ad793887c6450554839e3
Piotr Milewski <piotr250tiger@gmail.com>
2021-04-14 18:07:53 -0400

    clean up wolfie vip code that we didn't need

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,18 @@
 import React 			from 'react';
-import Homescreen 		from './components/homescreen/Homescreen';
-import { useQuery } 	from '@apollo/client';
-import * as queries 	from './cache/queries';
-import { jsTPS } 		from './utils/jsTPS';
+import Explorescreen 		from './components/explorescreen/Explorescreen';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
  
 const App = () => {
-	let user = null;
-    let transactionStack = new jsTPS();
-	
-    const { loading, error, data, refetch } = useQuery(queries.GET_DB_USER);
-
-    if(error) { console.log(error); }
-	if(loading) { console.log(loading); }
-	if(data) { 
-		let { getCurrentUser } = data;
-		if(getCurrentUser !== null) { user = getCurrentUser; }
-    }
 
 	return(
 		<BrowserRouter>
 			<Switch>
-				<Redirect exact from="/" to={ {pathname: "/home"} } />
+				<Redirect exact from="/" to={ {pathname: "/explore"} } />
 				<Route 
-					path="/home" 
-					name="home" 
+					path="/explore" 
+					name="explore" 
 					render={() => 
-						<Homescreen tps={transactionStack} fetchUser={refetch} user={user} />
+						<Explorescreen/>
 					} 
 				/>
 				<Route/>
---
+++ client/src/components/explorescreen/Explorescreen.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } 	from 'react';
+import Navbar_ 					from '../navbar/Navbar_';
+
+const Explorescreen = (props) => {
+
+	return (
+		<Navbar_/>
+	);
+};
+
+export default Explorescreen;
