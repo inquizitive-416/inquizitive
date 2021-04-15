734484aaab76d0512764b7e085b47803f296c168
LENOVO-PC\USER <faizanshaikh0303@gmail.com>
2021-04-14 20:33:53 -0400

    welcome screen started

---
+++ client/build/precache-manifest.70f533e667b9544fea842a8bbc174e36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "638995442f185dff0c2a3fadd5d3bd39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c439a3db8f9758bb2334",
+    "url": "/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "c439a3db8f9758bb2334",
+    "url": "/static/js/2.a4385939.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.a4385939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69b2e809ef1449d86208",
+    "url": "/static/js/main.1e1020c1.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
 import React 			from 'react';
 import Explorescreen 		from './components/explorescreen/Explorescreen';
+import Welcomescreen 		from './components/welcomescreen/Welcomescreen';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
  
 const App = () => {
 
 	return(
-		<BrowserRouter>
-			<Switch>
-				<Redirect exact from="/" to={ {pathname: "/explore"} } />
-				<Route 
-					path="/explore" 
-					name="explore" 
-					render={() => 
-						<Explorescreen/>
-					} 
-				/>
-				<Route/>
-			</Switch>
-		</BrowserRouter>
+		// render={() => 
+			<Welcomescreen/>
+		// } 
+		// <BrowserRouter>
+		// 	<Switch>
+		// 		<Redirect exact from="/" to={ {pathname: "/explore"} } />
+		// 		<Route 
+		// 			path="/explore" 
+		// 			name="explore" 
+		// 			render={() => 
+		// 				<Explorescreen/>
+		// 			} 
+		// 		/>
+		// 		<Route/>
+		// 	</Switch>
+		// </BrowserRouter>
 	);
 }
 
