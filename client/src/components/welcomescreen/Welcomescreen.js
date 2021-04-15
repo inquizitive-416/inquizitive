f4f7e7f79b55da45acc588df2621fdeab41eacf3
LENOVO-PC\USER <faizanshaikh0303@gmail.com>
2021-04-14 22:49:45 -0400

    welcome screen layout finished

--- client/src/components/welcomescreen/Welcomescreen.js
+++ client/src/components/welcomescreen/Welcomescreen.js
@@ -8,7 +8,7 @@ const Welcome = (props) => {
     const [password, setPassword] = useState("");
 
     function validateForm() {
-        return email.length > 0 && password.length > 0 && email.indexOf('@')>0;
+        return email.length > 0 && password.length > 0;
     }
 
     function handleSubmit(event) {
@@ -17,29 +17,46 @@ const Welcome = (props) => {
     }
 
     return (
-        <div className="Login">
-        <Form onSubmit={handleSubmit}>
-            <Form.Group size="lg" controlId="email">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-                autoFocus
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            </Form.Group>
-            <Form.Group size="lg" controlId="password">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            </Form.Group>
-            <Button block size="lg" type="submit" disabled={!validateForm()}>
-            Login
-            </Button>
-        </Form>
+        <div className="Welcome">
+            <div class="text" className="Inquizitive">
+                In<span style={{color: '#f5ae31'}}>Quiz</span>Itive
+            </div>
+            <div class="text" className="description">
+                Welcome to In<span style={{color: '#f5ae31'}}>Quiz</span>Itive, a place with all the tools needed to give every user the confidence to learn and succeed.
+            </div>
+            <div className="Login" >
+            <Form onSubmit={handleSubmit}>
+                <Form.Group size="lg" controlId="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                    autoFocus
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                </Form.Group>
+                <Form.Group size="lg" controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                </Form.Group>
+            
+                <div class="row">
+                <div class="col">
+                    <Button  style={{backgroundColor:"#f5ae31"}} block size="lg" disabled={!validateForm()} >
+                        Login
+                    </Button></div>
+                <div class="col">
+                    <Button  style={{backgroundColor:"#f5ae31"}} block size="lg" disabled={false}>
+                        Register
+                    </Button>
+                    </div>
+                </div>
+            </Form>
+            </div>
         </div>
     );
 };
