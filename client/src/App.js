8eb8efb8277caca3d47521c402a573cc31c8caf9
Faith Kern <faithkern@gmail.com>
2021-04-23 02:25:23 -0400

    Profile screen and route added, explore screen modified

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,38 @@
-import React from 'react';
-import Welcome from './components/welcomescreen/Welcomescreen';
-import Explorescreen from './components/explorescreen/Explorescreen';
-import CreateScreen from './components/createscreen/createScreen';
-import Settingsscreen from './components/settingsscreen/Settingsscreen';
-import Shopscreen from './components/shopscreen/Shopscreen';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
- 
-const App = () => {
+import React from "react";
+import Welcome from "./components/welcomescreen/Welcomescreen";
+import Explorescreen from "./components/explorescreen/Explorescreen";
+import CreateScreen from "./components/createscreen/createScreen";
+import Settingsscreen from "./components/settingsscreen/Settingsscreen";
+import Shopscreen from "./components/shopscreen/Shopscreen";
+import Profilescreen from "./components/profilescreen/Profilescreen";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
-	return(
-		<BrowserRouter>
-			<Switch>
-				<Redirect exact from="/" to={ {pathname: "/explore"} } />
-				<Route 
-					path="/explore" 
-					name="explore" 
-					render={() => 
-						<Explorescreen/>
-					} 
-				/>
-				<Route 
-					path="/welcome" 
-					name="welcome" 
-					render={() => 
-						<Welcome/>
-					} 
-				/>
-				<Route 
-					path="/create" 
-					name="create" 
-					render={() => 
-						<CreateScreen/>
-					} 
-				/>
-				<Route 
-					path="/settings" 
-					name="settings" 
-					render={() => 
-						<Settingsscreen/>
-					} 
-				/>
-				<Route 
-					path="/shop" 
-					name="shop" 
-					render={() => 
-						<Shopscreen/>
-					} 
-				/>
-			</Switch>
-		</BrowserRouter>
-	);
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Redirect exact from="/" to={{ pathname: "/explore" }} />
+        <Route
+          path="/explore"
+          name="explore"
+          render={() => <Explorescreen />}
+        />
+        <Route path="/welcome" name="welcome" render={() => <Welcome />} />
+        <Route path="/create" name="create" render={() => <CreateScreen />} />
+        <Route
+          path="/settings"
+          name="settings"
+          render={() => <Settingsscreen />}
+        />
+        <Route path="/shop" name="shop" render={() => <Shopscreen />} />
+        <Route
+          path="/profile"
+          name="profile"
+          render={() => <Profilescreen />}
+        />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
