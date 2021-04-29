7ee81fa68e192cb1d05ea61ff99e5670311ae245
Faith Kern <faithkern@gmail.com>
2021-04-29 11:13:31 -0400

    Removed quiz-resolvers and quiz-def changes

--- typedefs/quiz-def.js
+++ typedefs/quiz-def.js
@@ -2,73 +2,71 @@ const { gql } = require("apollo-server");
 /** typedefs for Quiz **/
 
 const typeDefs = gql`
-  type Quiz {
-    _id: String
-    idOfCreator: String
-    title: String
-    description: String
-    coverimage: String
-    categories: String
-    hashtagone: String
-    hashtagtwo: String
-    hashtagthree: String
-    difficulty: String
-    quizposted: Boolean
-    timer: Int
-    questions: [String]
-    ratings: Int
-    avgRating: Int
-    numOfTimesPlayed: Int
-    isReported: Boolean
-  }
+	type Quiz {
+		_id: String
+		idOfCreator: String
+        title:String
+		description:String
+		coverimage:String
+		categories:String
+		hashtagone:String
+		hashtagtwo:String
+		hashtagthree:String
+		difficulty: String
+		quizposted: Boolean
+		timer: Int
+		questions: [String]
+		ratings: Int
+		avgRating: Int
+		numOfTimesPlayed: Int
+		isReported: Boolean
+	}
 
-  extend type Query {
-    getQuizById(_id: String!): Quiz
-    getQuizzesByRating: [Quiz]
-  }
-  extend type Mutation {
-    addQuiz(
-      idOfCreator: String
-      title: String
-      description: String
-      coverimage: String
-      categories: String
-      hashtagone: String
-      hashtagtwo: String
-      hashtagthree: String
-      difficulty: String
-      quizposted: Boolean
-      timer: Int
-      questions: [String]
-      ratings: Int
-      avgRating: Int
-      numOfTimesPlayed: Int
-      isReported: Boolean
-    ): Quiz
+	extend type Query {
+		getQuizById(_id: String!): Quiz
+	}
+	extend type Mutation {
+		addQuiz(
+			idOfCreator: String
+			title:String
+			description:String
+			coverimage:String
+			categories:String
+			hashtagone:String
+			hashtagtwo:String
+			hashtagthree:String
+			difficulty: String
+			quizposted: Boolean
+			timer: Int
+			questions: [String]
+			ratings: Int
+			avgRating: Int
+			numOfTimesPlayed: Int
+			isReported: Boolean): Quiz
 
-    deleteQuiz(_id: String!): Boolean
-    updateQuizField(_id: String!, field: String!, value: String!): Boolean
-  }
+		deleteQuiz(_id: String!): Boolean
+		updateQuizField(_id: String!, field: String!, value: String!): Boolean
+	}
 
-  input QuizInput {
-    _id: String
-    idOfCreator: String
-    title: String
-    description: String
-    coverimage: String
-    categories: String
-    hashtagone: String
-    hashtagtwo: String
-    hashtagthree: String
-    difficulty: String
-    quizposted: Boolean
-    timer: Int
-    questions: [String]
-    ratings: Int
-    avgRating: Int
-    numOfTimesPlayed: Int
-    isReported: Boolean
-  }
+	input QuizInput {
+		_id: String
+		idOfCreator: String
+        title:String
+		description:String
+		coverimage:String
+		categories:String
+		hashtagone:String
+		hashtagtwo:String
+		hashtagthree:String
+		difficulty:String
+		quizposted: Boolean
+		timer: Int
+		questions: [String]
+		ratings: Int
+		avgRating: Int
+		numOfTimesPlayed: Int
+		isReported: Boolean
+	}
 `;
 
 module.exports = { typeDefs: typeDefs };
