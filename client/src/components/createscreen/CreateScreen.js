569eb4d0710a4fd2f9c91bb6dee932087092e063
Sara Agarwal <38178032+saagarwal13@users.noreply.github.com>
2021-05-15 15:35:33 -0400

    alert added for incomplete quiz

--- client/src/components/createscreen/CreateScreen.js
+++ client/src/components/createscreen/CreateScreen.js
@@ -9,8 +9,10 @@ import { Navbar, Nav } from 'react-bootstrap'
 import { Redirect } from "react-router-dom"
 import { uploadFile } from 'react-s3'
 import { getCurrentUser } from "../../data/LocalStorage"
+import Toast from 'react-bootstrap/Toast'
 
 import { GET_CURRENT_USER } from '../settingsscreen/queries'
+import SubmitModal from "./SubmitModal";
 
 import { Card, Button, Form, Col, Row } from 'react-bootstrap'
 
@@ -22,6 +24,7 @@ const CreateScreenSub = (props) => {
 
     
     const [questions , setQuestions] = useState([])
+    const [submodalShow, setSubmodalShow] = useState(false);
     const [showques , setshowques] = useState(true)
     const [showAdd , setShowAdd] = useState(false)
     const [gotoexplore, setgoto]= useState(false)
@@ -51,7 +54,11 @@ const CreateScreenSub = (props) => {
 
 
     const [addQuiz]= useMutation(ADDQUIZ)
-
+     
+    const subModal=()=>{
+        
+        setSubmodalShow(!submodalShow);
+    }
       
     const handleNewImage = (e) => {
         
@@ -63,6 +70,7 @@ const CreateScreenSub = (props) => {
 
         setImage(renamedImage);
     }
+
 
     const uploadNewImage = async (e) => {
         
@@ -146,11 +154,27 @@ const CreateScreenSub = (props) => {
              console.log("quess", quizInfo.questions)
      }
 
+     const onCheckValid = () =>
+     {
+         if (quizInfo.title === "" || quizInfo.description === "" || quizInfo.coverimage === "" || quizInfo.categories === "")
+         {
+             subModal()
+
+         }
+     }
+
      const onSubmit= async(e)=>{
         e.preventDefault();
         
         //setQuizInfo({...quizInfo, questions: allQuestions});
         //console.log(" here questions", allQuestions)
+        if (quizInfo.title === "" || quizInfo.description === "" || quizInfo.coverimage === "" || quizInfo.categories === "")
+         {
+             setSubmodalShow(true)
+
+         }
+        else
+        {
 
         
        
@@ -162,6 +186,7 @@ const CreateScreenSub = (props) => {
         }
         setgoto(true)
     }
+    }
 
     const onAdd = ()=>
     {
@@ -172,7 +197,7 @@ const CreateScreenSub = (props) => {
 	return (
         <div>
 		<NavbarTop fetchUser={props.fetchUser}/>
-            <div  className ="create"  style={{overflow:"scroll"}}>
+            <div  className ="create"  style={{overflow:"scroll", position: "absolute"}}>
                 <div style={{backgroundColor: "#484848", height: 50, paddingBottom: 90, textAlign: "center"}} >
             
                     <header style= {{paddingTop:25, fontSize: 35, color:"#FFA500" }}> Create New Quiz  </header>
@@ -247,9 +272,25 @@ const CreateScreenSub = (props) => {
                   <button style= {{backgroundColor: "#ffa343", width: 915}}  onClick={onAdd} class="btn btn-primary" type="button"> <i class="fa fa-plus"></i> Add   New   Question</button>
                   {showAdd && <AddQuestion  addQues ={addQues} onAdd = {onAdd}/>}
                </div>
+
                <div style={{paddingTop:40 , paddingLeft:600}}>
+
+                <Toast show={submodalShow} onClose={subModal}>
+                <Toast.Header>
+                    <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
+                    <strong className="mr-auto">Warning</strong>
+                    <small>11 mins ago</small>
+                </Toast.Header>
+                <Toast.Body>Fill in missing fields before creating quiz</Toast.Body>
+                </Toast>
+
+               </div>
+
+
+               <div style={{ paddingBottom:30, paddingLeft:600}}>
+
                {renderRedirect()}
-               <button style= {{backgroundColor: "orange"}} onClick = {onSubmit} class= "btn btn-primary" >Submit quiz</button>
+               <button style= {{backgroundColor: "orange"}} onClick = {onSubmit} class= "btn btn-primary" >Create quiz</button>
 
                </div>
               
@@ -279,8 +320,11 @@ const CreateScreen = (props) => {
     //console.log(currentUser)
 
 	return (
-		
+        
+        <div style={{minHeight:"100vh"}} >
         <CreateScreenSub user={currentUser}/>
+        </div>
+        
             
 	);
 };
