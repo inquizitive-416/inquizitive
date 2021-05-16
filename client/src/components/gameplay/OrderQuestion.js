726bd12ecb85382acbbaad318b984beb698dc57f
LENOVO-PC\USER <faizanshaikh0303@gmail.com>
2021-05-15 23:49:01 -0400

    order half done

--- client/src/components/gameplay/OrderQuestion.js
+++ client/src/components/gameplay/OrderQuestion.js
@@ -1,110 +1,59 @@
-import React from 'react'
+import React,{useState} from 'react';
 import { DragDropContext,Droppable, Draggable } from 'react-beautiful-dnd';
 import {Card} from 'react-bootstrap'
+
 const OrderQuestion=(props)=>{
     const myArr=["Title1","Title2","Title3","Title4"]
-    return(
-        //<div>
-            <DragDropContext>
-                <Droppable>
-                    {(provided)=>(
-
-                        <div  ref={provided.innerRef}
-                        {...provided.droppableProps}
-                        >
-                            {
-                                myArr.map((title,index)=>{
-                                    return(
-                                        <Draggable draggableId={index+1} key={index+1} index={index}>
-                                        {(provided,snapshot) => (
-                                            //<div class='col' >
-                                                <Card style={{width:'100%',height:'100%'}} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                                                    <Card.Body>
-                                                        <Card.Title>
-                                                            {title}
-                                                        </Card.Title>
-                                                        <Card.Text>
-                                                            {title}
-                                                        </Card.Text>
-                                                    </Card.Body>
-                                                </Card>
-                                            //</div>
-                                        )}
-                                        </Draggable>
-                                    )
-                                })
-                            }
-                            {/* <Draggable draggableId={1} key={1} index={0}>
-                            {(provided) => (
-                                //<div class='col' >
-                                    <Card style={{width:'100%',height:'100%'}} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                                        <Card.Body>
-                                            <Card.Title>
-                                                Title 1
-                                            </Card.Title>
-                                            <Card.Text>
-                                                Description 1
-                                            </Card.Text>
-                                        </Card.Body>
-                                    </Card>
-                                //</div>
-                            )}
-                            </Draggable> */}
-                            {/* <Draggable draggableId={2} key={2} index={1}>
-                            {(provided) => (
-                                //<div class='col' >
-                                    <Card style={{width:'100%',height:'100%'}} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                                        <Card.Body>
-                                            <Card.Title>
-                                                Title 2
-                                            </Card.Title>
-                                            <Card.Text>
-                                                Description 2
-                                            </Card.Text>
-                                        </Card.Body>
-                                    </Card>
-                                //</div>
-                            )}
-                            </Draggable>
-                            <Draggable draggableId={3} key={3} index={2}>
-                            {(provided) => (
-                                //<div class='col' >
-                                    <Card style={{width:'100%',height:'100%'}} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                                        <Card.Body>
-                                            <Card.Title>
-                                                Title 3
-                                            </Card.Title>
-                                            <Card.Text>
-                                                Description 3
-                                            </Card.Text>
-                                        </Card.Body>
-                                    </Card>
-                                //</div>
-                            )}
-                            </Draggable>
-                            <Draggable draggableId={4} key={4} index={3}>
-                            {(provided) => (
-                                //<div class='col' >
-                                    <Card style={{width:'100%',height:'100%'}} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                                        <Card.Body>
-                                            <Card.Title>
-                                                Title 4
-                                            </Card.Title>
-                                            <Card.Text>
-                                                Description 4
-                                            </Card.Text>
-                                        </Card.Body>
-                                    </Card>
-                                //</div>
-                            )}
-                            </Draggable> */}
+    const [characters, updateCharacters] = useState(myArr);
+    const onDragEnd=(result)=>{
+        if (!result.destination) return;
+    
+        const items = Array.from(characters);
+        const [reorderedItem] = items.splice(result.source.index, 1);
+        items.splice(result.destination.index, 0, reorderedItem);
+    
+        updateCharacters(items);
+    }
+    return (
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="droppable" direction="horizontal">
+            {(provided) => (
+              <div
+                class="row"
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                style = {{backgroundColor:"grey",height:"50vh",borderColor:"red",border:"50px"}}
+                // style={getListStyle(snapshot.isDraggingOver)}
+              >
+                {characters.map((item, index) => (
+                  <Draggable key={item} draggableId={item} index={index}>
+                    {(provided) => (
+                    <div class="col" style={{padding:20,width:200, height:200}}>
+                        <Card
+                            // style={{height:200}}
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
                             
-                        </div>
+                        >
+                            <Card.Body>
+                                <Card.Title>
+                                    {item}
+                                </Card.Title>
+                                <Card.Text>
+                                    {item}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
                     )}
-                    
-                </Droppable>
-            </DragDropContext>
-        //</div>
-    );
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      );
 }
 export default OrderQuestion;
